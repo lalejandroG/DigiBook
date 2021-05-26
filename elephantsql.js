cc2b45fca2f78a677aa35a1c2fad4f0885242b16
Esther Sanchez <esther.sanchez@correo.unimet.edu.ve>
2021-05-26 10:40:13 -0400

    funcion registro

--- elephantsql.js
+++ elephantsql.js
@@ -1,17 +1,44 @@
-var pg = require('pg');
+//var pg = require('pg');
 
-var conString = "postgres://bhqbaqpt:2WbvlNpZb6V6BupIHAV8G7eUlTASlxQh@kashin.db.elephantsql.com/bhqbaqpt" //Can be found in the Details page
-var client = new pg.Client(conString);
-client.connect(function(err) {
-  if(err) {
-    return console.error('No se pudo conectar a postgres', err);
-  }
-  client.query('SELECT * FROM autor', function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0]);
+//var conString = "postgres://bhqbaqpt:2WbvlNpZb6V6BupIHAV8G7eUlTASlxQh@kashin.db.elephantsql.com/bhqbaqpt" //Can be found in the Details page
+//var client = new pg.Client(conString);
+//client.connect(function(err) {
+  //if(err) {
+    //return console.error('No se pudo conectar a postgres', err);
+  //}
+  //client.query('SELECT * FROM autor', function(err, result) {
+    //if(err) {
+      //return console.error('error running query', err);
+    //}
+    //console.log(result.rows[0]);
     // >> output: 2018-08-23T14:02:57.117Z
-    client.end();
-  });
+    //client.end();
+  //});
+//});
+
+require("dotenv").config();
+
+const { Pool } = require("pg");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const connectionString = 'postgres://$(process.env.DB_USER):$(process.env.DB_PASSWORD)@$(process.env.DB_HOST):$(process.env.DB_PORT)/$(process.env.DB_DATABASE)';
+
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString
+
 });
+
+module.exports = { pool };
+
+
+
+
+
+
+
+
+
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const flash = require('express-flash');
+
+
+const { pool } = require('path').basename('/elephantsql');
+
+router.use(express.urlencoded({ extended: false }));
+
+router.use(session({
+  secret: "secret",
+  resave: false,
+  saveUninitialized: false
+}));
+
+router.use(flash());
 
 /* GET home page. */
 router.get('/', (req, res) => {
@@ -13,7 +30,53 @@ router.get('/login', (req, res) => {
 
 /* GET signup page. */
 router.get('/signup', (req, res) => {
-  res.render('signup.html', { title: 'signup' });
+  res.render('signup.html');
 });
+
+router.post('/signup', async (req, res) => {
+  let { email, username, password, fotoPerfil, biografia } = req.body;
+  console.log({
+    email,
+    username,
+    password,
+    fotoPerfil,
+    biografia
+  });
+  let errors = [];
+
+  if (errors.lenght > 0){
+    res.render('signup.html', { errors });
+  }else{
+    let hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+
+    pool.query(
+      'SELECT * FROM cuenta WHERE correo = $1', [email], (err, results) => {
+        if (err){
+          throw err
+        }
+
+        console.log(results.rows);
+
+        if (results.rows.lenght > 0){
+          errors.push({message: "Ese correo ya ha sido registrado"});
+          res.render('signup.html', { errors });
+        }else{
+          pool.query(
+            'INSERT INTO cuenta (correo, username, password, imagen_perfil, biografia) VALUES ($1, $2, $3, $4, $5) RETURNING id_cuenta, password', [email, username, hashedPassword, fotoPerfil, biografia], (err, results) => {
+                if (err){
+                  throw err
+                }
+                console.log(results.rows);
+                req.flash('exito', "Ha sido registrado.");
+                res.redirect('/login');
+            }
+          );
+        }
+      }
+    );
+  }
+});
+
 
 module.exports = router;
