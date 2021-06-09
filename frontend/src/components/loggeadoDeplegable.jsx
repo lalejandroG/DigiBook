fbfa16708797f7d0b066fea3d0e6d58890440c4b
Carla Rodriguez <carlyta99@gmail.com>
2021-06-09 11:50:18 -0400

    Cerrar sesion

--- frontend/src/components/loggeadoDeplegable.jsx
+++ frontend/src/components/loggeadoDeplegable.jsx
@@ -1,6 +1,7 @@
 import styles from "../styles/menu.module.css";
 import {Link} from "@material-ui/core";
 import AccountCircle from "@material-ui/icons/AccountCircle";
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import React from "react";
 
 export default function Loggeado (){
@@ -10,9 +11,16 @@ export default function Loggeado (){
                 <Link href="../pages/profile">
                     <a href="/profile" >
                         <a className={styles.prueba}>
-                         <i className={styles.materialIcons} >account_circle</i>
+                            <i className={styles.materialIcons} >account_circle</i>
                         </a>
 
+                    </a>
+                </Link>
+                <Link href="../pages/landingPage">
+                    <a href="/" >
+                        <a className={styles.prueba}>
+                            <i className={styles.materialIcons} >exit_to_app</i>
+                        </a>
                     </a>
                 </Link>
             </div>
