e63fc0784d24502085b41cbdbc1f7ea71dc3ebcd
Simon Mesber <simon.mesber@correo.unimet.edu.ve>
2021-07-05 00:27:19 -0400

    PWA funcional

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {FirebaseAppProvider} from "reactfire";
 import firebaseConfig from "./firebaseConfig";
-import ServiceWorker from "./ServiceWorker"
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render((
         <FirebaseAppProvider firebaseConfig={firebaseConfig}>
@@ -24,4 +24,4 @@ ReactDOM.render((
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-ServiceWorker();
+serviceWorkerRegistration.register();
